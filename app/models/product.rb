7ef6718cf3684df9a7006f4c1ca3fa461a4315a6
Nina Breznik <ninabreznik@gmail.com>
2013-10-18 14:50:06 +0200

    validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ActiveRecord::Base
+
+  validates :name,  presence: true
+  VALID_PRICE_REGEX = /^\d+(([.,])\d+)?$/
+  validates :price, presence: true, format: { with: VALID_PRICE_REGEX }
 end
